dce9f49ead2c58648f4a36161df48c95bbf36b53
JinSeoHyun <15roa1121@gmail.com>
2024-05-25 17:52:15 +0900

    스프링 시큐리티 설정하기 버전 6.2.4

---
+++ src/main/java/com/jsh/securitystudy/controller/IndexRestController.java
@@ -0,0 +1,13 @@
+package com.jsh.securitystudy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexRestController {
+
+//    @GetMapping({"/",""})
+    public String index() {
+        return "index";
+    }
+}
