4d538febb60b09970f8aca7c0549fd76857e82d6
JinSeoHyun <15roa1121@gmail.com>
2024-05-27 01:53:48 +0900

    시큐리티 로그인 - PrincipalDetailsService 만들기

--- src/main/java/com/jsh/securitystudy/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/jsh/securitystudy/config/auth/PrincipalDetailsService.java
@@ -1,2 +1,20 @@
-package com.jsh.securitystudy.config.auth;public class PrincipalDetailsService {
+package com.jsh.securitystudy.config.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/*
+시큐리티 설정에서 loginProcessingUrl("/login");
+-> login 요청이 오면 자동으로 UserDetailsService 타입으로 IoC되어 있는 loadUserByUsername 실행~!!!
+*/
+@Service
+public class PrincipalDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return null;
+    }
 }
